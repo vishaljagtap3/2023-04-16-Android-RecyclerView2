732fb5c8a1841f5bbe181ff3876bc8da9dfa2197
vishaljagtap3 <vishal.jagtap@bitcode.in>
2023-04-20 14:13:47 +0530

    Part 2

--- app/src/main/java/com/bitcodetech/recyclerview2/PostsAdapter.kt
+++ app/src/main/java/com/bitcodetech/recyclerview2/PostsAdapter.kt
@@ -1,5 +1,6 @@
 package com.bitcodetech.recyclerview2
 
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -7,20 +8,31 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class PostsAdapter(
-    private val posts : ArrayList<Post>
-) : RecyclerView.Adapter<PostsAdapter.PostViewHolder>(){
+    private val posts: ArrayList<Post>
+) : RecyclerView.Adapter<PostsAdapter.PostViewHolder>() {
 
-    inner class PostViewHolder(view : View) : RecyclerView.ViewHolder(view) {
 
-        val txtAccountName : TextView
-        val txtPostedOn : TextView
-        val imgPost : ImageView
-        val btnLike : Button
-        val txtLikes : TextView
-        val txtText : TextView
+    interface OnPostClickListener {
+        fun onAccountClick(accountName: String, position: Int)
+        fun onImageClick(imageId: Int, position: Int)
+        fun onLikeClicked(id: Int, position: Int)
+    }
+
+    var onPostClickListener: OnPostClickListener? = null
+
+
+    inner class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val txtAccountName: TextView
+        val txtPostedOn: TextView
+        val imgPost: ImageView
+        val btnLike: Button
+        val txtLikes: TextView
+        val txtText: TextView
 
         init {
             txtAccountName = view.findViewById(R.id.txtAccountName)
@@ -31,9 +43,34 @@ class PostsAdapter(
             txtText = view.findViewById(R.id.txtText)
 
             btnLike.setOnClickListener {
-                posts[adapterPosition].likes += 1
-                txtLikes.text = "${posts[adapterPosition].likes}"
+                /*posts[adapterPosition].likes += 1
+                txtLikes.text = "${posts[adapterPosition].likes}"*/
+
+                onPostClickListener?.onLikeClicked(
+                    posts[adapterPosition].id,
+                    adapterPosition
+                )
+
             }
+
+            imgPost.setOnClickListener {
+                //mt(it.context, "Image clicked: $adapterPosition")
+                //mt(it.context, "image ${adapterPosition} liked")
+                onPostClickListener?.onImageClick(
+                    posts[adapterPosition].imageId!!,
+                    adapterPosition
+                )
+            }
+
+            txtAccountName.setOnClickListener {
+                //mt(it.context, "${posts[adapterPosition].accountName} clicked")
+                onPostClickListener?.onAccountClick(
+                    posts[adapterPosition].accountName,
+                    adapterPosition
+                )
+            }
+
+
         }
     }
 
@@ -53,17 +90,20 @@ class PostsAdapter(
 
         holder.txtAccountName.text = post.accountName
         holder.txtPostedOn.text = post.postedOn
-        if(post.imageId != null) {
+        if (post.imageId != null) {
             holder.imgPost.setImageResource(post.imageId)
         }
         holder.txtLikes.text = "${post.likes}"
-        if(post.text != null) {
+        if (post.text != null) {
             holder.txtText.text = post.text
             holder.txtText.visibility = View.VISIBLE
-        }
-        else {
+        } else {
             holder.txtText.visibility = View.GONE
         }
+    }
+
+    private fun mt(context: Context, text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/bitcodetech/recyclerview2/SavedPostsActivity.kt
+++ app/src/main/java/com/bitcodetech/recyclerview2/SavedPostsActivity.kt
@@ -1,19 +1,16 @@
 package com.bitcodetech.recyclerview2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class SavedPostsActivity : AppCompatActivity() {
 
     private lateinit var recyclerPosts : RecyclerView
     private val posts = ArrayList<Post>();
     private lateinit var postsAdapter : PostsAdapter
-    private lateinit var btnStartSavePostsActivity : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +21,6 @@ class MainActivity : AppCompatActivity() {
         initData()
 
         initViews()
-        setupListeners()
-    }
-
-    private fun setupListeners() {
-        btnStartSavePostsActivity.setOnClickListener {
-            startActivity(
-                Intent(this@MainActivity, SavedPostsActivity::class.java)
-            )
-        }
     }
 
     private fun initViews() {
@@ -46,29 +34,23 @@ class MainActivity : AppCompatActivity() {
         postsAdapter = PostsAdapter(posts)
         recyclerPosts.adapter = postsAdapter
 
-        postsAdapter.onPostClickListener = AllPostsOnClickListener()
-
-        btnStartSavePostsActivity = findViewById(R.id.btnStartSavePostActivity)
+        postsAdapter.onPostClickListener = SavePostsClickListener()
     }
 
-    inner class AllPostsOnClickListener : PostsAdapter.OnPostClickListener {
+    inner class SavePostsClickListener : PostsAdapter.OnPostClickListener {
         override fun onAccountClick(accountName: String, position: Int) {
-            mt("MA: account at $position name: $accountName clicked")
+            mt("SPA: account at $position name: $accountName clicked")
         }
 
         override fun onImageClick(imageId: Int, position: Int) {
-            mt("MA: image at $position clicked")
+            mt("SPA: image at $position clicked")
         }
 
         override fun onLikeClicked(id: Int, position: Int) {
-            mt("MA: post at $position liked")
+            mt("SPA: post at $position liked")
             posts[position].likes += 1
         }
 
-    }
-
-    private fun mt(text : String) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 
     private fun initData() {
@@ -117,6 +99,10 @@ class MainActivity : AppCompatActivity() {
         posts.add(
             Post(1, "@bitcodetech-android", "12-04-2023", R.drawable.image01, "Are you looking to build Career in IT ?\n" + "Join BitCode! We are the Best IT Training Company in Pune.", 10)
         )
+    }
+
+    private fun mt(text : String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
 
